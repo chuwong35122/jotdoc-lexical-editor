ef95bacbb2a5d7256df273d7dcfbab9b2d5824a0
ChuChuwiiii <chuwong35122@gmail.com>
2023-09-19 10:45:25 +0700

    feat: add rollup & build the components

---
+++ dist/esm/types/components/Buttons/MainButton/MainButton.d.ts
@@ -0,0 +1,5 @@
+import React, { HTMLAttributes } from 'react';
+export interface MainButtonProps extends HTMLAttributes<HTMLButtonElement> {
+}
+declare function MainButton(props: MainButtonProps): React.JSX.Element;
+export default MainButton;
---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+import React, { HTMLAttributes } from 'react';
+
+interface MainButtonProps extends HTMLAttributes<HTMLButtonElement> {
+}
+declare function MainButton(props: MainButtonProps): React.JSX.Element;
+
+export { MainButton };

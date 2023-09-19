228eb35a140b58ff177fcf0850cbff4036df3786
ChuChuwiiii <chuwong35122@gmail.com>
2023-09-19 10:48:35 +0700

    remove gitignore files

--- dist/esm/types/components/Buttons/MainButton/MainButton.d.ts
+++
@@ -1,5 +0,0 @@
-import React, { HTMLAttributes } from 'react';
-export interface MainButtonProps extends HTMLAttributes<HTMLButtonElement> {
-}
-declare function MainButton(props: MainButtonProps): React.JSX.Element;
-export default MainButton;
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { HTMLAttributes } from 'react';
-
-interface MainButtonProps extends HTMLAttributes<HTMLButtonElement> {
-}
-declare function MainButton(props: MainButtonProps): React.JSX.Element;
-
-export { MainButton };

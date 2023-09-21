3dc6d648b092a2f0ee5de174d16c539d6ffd86be
ChuChuwiiii <chuwong35122@gmail.com>
2023-09-21 14:31:26 +0700

    feat: prepare constants for Editor's Toolbar

--- packages/src/constants/themes.ts
+++ packages/src/constants/themes.ts
@@ -12,3 +12,126 @@ export const THEME_COLORS = {
     option: "rgba(0, 125, 255, 0.2)",
   },
 };
+
+export const DEFAULT_TEXT: {
+  color: string;
+  fontSize: string;
+  fontFamily: string;
+  highlight: string;
+} = {
+  color: "rgba(0,0,0,1)",
+  fontSize: "16px",
+  fontFamily: "Arial",
+  highlight: "rgba(0,0,0,0)",
+};
+
+export const FONT_SIZE_OPTIONS = [
+  "8px",
+  "10px",
+  "12px",
+  "14px",
+  "16px",
+  "18px",
+  "20px",
+  "24px",
+  "30px",
+  "36px",
+  "48px",
+  "60px",
+  "72px",
+  "96px",
+];
+
+export const FONT_FAMILY_OPTIONS: string[] = [
+  "Roboto",
+  "Arial",
+  "Courier New",
+  "Georgia",
+  "Times New Roman",
+  "Trebuchet MS",
+  "Verdana",
+  "Menlo",
+];
+
+export const COLOR_OPTIONS = [
+  {
+    name: "Red",
+    value: "rgba(255, 0, 0, 1)",
+  },
+  {
+    name: "Cyan",
+    value: "rgba(0, 255, 255, 1)",
+  },
+  {
+    name: "Blue",
+    value: "rgba(0, 0, 255, 1)",
+  },
+  {
+    name: "DarkBlue",
+    value: "rgba(0, 0, 139, 1)",
+  },
+  {
+    name: "LightBlue",
+    value: "rgba(173, 216, 230, 1)",
+  },
+  {
+    name: "Purple",
+    value: "rgba(128, 0, 128, 1)",
+  },
+  {
+    name: "Yellow",
+    value: "rgba(255, 255, 0, 1)",
+  },
+  {
+    name: "Lime",
+    value: "rgba(0, 255, 0, 1)",
+  },
+  {
+    name: "Magenta",
+    value: "rgba(255, 0, 255, 1)",
+  },
+  {
+    name: "Pink",
+    value: "rgba(255, 192, 203, 1)",
+  },
+  {
+    name: "White",
+    value: "rgba(255, 255, 255, 1)",
+  },
+  {
+    name: "Silver",
+    value: "rgba(192, 192, 192, 1)",
+  },
+  {
+    name: "Grey",
+    value: "rgba(128, 128, 128, 1)",
+  },
+  {
+    name: "Black",
+    value: "rgba(0, 0, 0, 1)",
+  },
+  {
+    name: "Orange",
+    value: "rgba(255, 165, 0, 1)",
+  },
+  {
+    name: "Brown",
+    value: "rgba(165, 42, 42, 1)",
+  },
+  {
+    name: "Maroon",
+    value: "rgba(128, 0, 0, 1)",
+  },
+  {
+    name: "Green",
+    value: "rgba(0, 128, 0, 1)",
+  },
+  {
+    name: "Olive",
+    value: "rgba(128, 128, 0, 1)",
+  },
+  {
+    name: "Aquamarine",
+    value: "rgba(127, 255, 212, 1)",
+  },
+];

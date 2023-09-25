196e29f625fd334109c153665803ebadf97f8063
ChuChuwiiii <chuwong35122@gmail.com>
2023-09-25 16:46:34 +0700

    feat: add text format commands

--- packages/src/components/Toolbar/EditorToolbar/EditorToolbar.tsx
+++ packages/src/components/Toolbar/EditorToolbar/EditorToolbar.tsx
@@ -9,8 +9,9 @@ import FontSizeSelector from "../../FontSizeSelector/FontSizeSelector";
 import { FONT_SIZE_OPTIONS } from "../../../constants/themes";
 import ColorPallette from "../../ColorPallette/ColorPallette";
 import { RGBColor } from "react-color";
-import { REDO_COMMAND, UNDO_COMMAND } from "lexical";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { $getSelection, $isRangeSelection, FORMAT_TEXT_COMMAND, REDO_COMMAND, UNDO_COMMAND } from "lexical";
+import { $patchStyleText } from '@lexical/selection';
 
 interface EditorToolbarProps { }
 
@@ -28,12 +29,34 @@ function EditorToolbar(props: EditorToolbarProps) {
   function handleSetHighlighterColor(color: RGBColor) {
     color.a = 0.5;
     const _color = `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`;
+    selectHighlighterColor(_color)
     context.setHighlightedColor(_color);
   }
 
   function setFontColor(color: RGBColor) {
     const _color = `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`;
+    selectColor(_color);
     context.setFontColor(_color);
+  }
+
+
+  function selectColor(target: string) {
+    editor.update(() => {
+      const selection = $getSelection();
+      if ($isRangeSelection(selection)) {
+        $patchStyleText(selection, { color: target });
+      }
+    });
+  }
+
+
+  function selectHighlighterColor(target: string) {
+    editor.update(() => {
+      const selection = $getSelection();
+      if ($isRangeSelection(selection)) {
+        $patchStyleText(selection, { 'background-color': target });
+      }
+    });
   }
 
   return (
@@ -70,26 +93,30 @@ function EditorToolbar(props: EditorToolbarProps) {
       <IconToggle
         iconSrc={"/icons/bold.svg"}
         state={context.isBold}
+        onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold')}
         name="bold"
       />
       <IconToggle
         iconSrc={"/icons/italic.svg"}
         state={context.isItalic}
+        onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'italic')}
         name="italic"
       />
       <IconToggle
         iconSrc={"/icons/underline.svg"}
         state={context.isUnderline}
+        onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'underline')}
         name="underline"
       />
       <IconToggle
         iconSrc={"/icons/strikethrough.svg"}
         state={context.isStrikethrough}
+        onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'strikethrough')}
         name="strikethrough"
       />
       <ColorPallette
         iconSrc="/icons/baseline.svg"
-        name="Color"
+        name="color"
         activeColor={context.fontColor}
         isOpen={colorOpen}
         setIsOpen={setColorOpen}
@@ -97,7 +124,7 @@ function EditorToolbar(props: EditorToolbarProps) {
       />
       <ColorPallette
         iconSrc="/icons/highlighter.svg"
-        name="Highlighter"
+        name="background-color"
         activeColor={context.highlightedColor}
         isOpen={highlighterOpen}
         setIsOpen={setHighlighterOpen}

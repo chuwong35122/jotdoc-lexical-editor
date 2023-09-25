4a6fdc2289bc5f0e9e09b9b5fdc29d4d28d00d33
ChuChuwiiii <chuwong35122@gmail.com>
2023-09-25 14:12:24 +0700

    feat: add colors to color pallettes

--- packages/src/components/ColorPallette/ColorPallette.tsx
+++ packages/src/components/ColorPallette/ColorPallette.tsx
@@ -1,11 +1,5 @@
-import React, {
-  Dispatch,
-  SetStateAction,
-  useEffect,
-  useRef,
-} from "react";
+import React, { Dispatch, SetStateAction, useEffect, useRef } from "react";
 import { GithubPicker, RGBColor } from "react-color";
-import { ReactSVG } from "react-svg";
 import "./ColorPallette.css";
 
 interface ColorPalletteProps {
@@ -13,12 +7,12 @@ interface ColorPalletteProps {
   name: string;
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
-  activeColor: string
+  activeColor: string;
   onSelect?: (color: RGBColor) => void;
 }
 
 function ColorPallette(props: ColorPalletteProps) {
-  const { iconSrc, activeColor, isOpen, setIsOpen, onSelect } = props;
+  const { iconSrc, activeColor, name, isOpen, setIsOpen, onSelect } = props;
   const selector = useRef<HTMLDivElement>(null);
 
   function handleClickButton() {
@@ -42,11 +36,29 @@ function ColorPallette(props: ColorPalletteProps) {
   return (
     <div ref={selector}>
       <button className="toggle-container" onClick={handleClickButton}>
-        <ReactSVG src={iconSrc} color={activeColor} />
+        <img src={iconSrc} width={24} height={24} alt={name} />
       </button>
       <div className="picker-dropdown">
         {isOpen && (
           <GithubPicker
+            colors={[
+              "rgba(255,0,0,1)",
+              "rgba(232,20,76,1)",
+              "rgba(233,30,99,1)",
+              "rgba(252,185,0,1)",
+              "rgba(139,195,74,1)",
+              "rgba(0,139,2,1)",
+              "rgba(0,107,118,1)",
+              "rgba(18,115,222,1)",
+              "rgba(0,77,207,1)",
+              "rgba(83,0,235,1)",
+              "rgba(103,57,183,1)",
+              "rgba(219,62,0,1)",
+              "rgba(121,85,72,1)",
+              "rgba(255,255,255,1)",
+              "rgba(171,184,195,1)",
+              "rgba(0,0,0,1)",
+            ]}
             color={activeColor}
             onChange={(c, _) => onSelect?.(c.rgb)}
           />
